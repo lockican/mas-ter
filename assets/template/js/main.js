730ae3b02b9979cf6f0dcda9636071b8af9a79c0
locvi <locvi@yandex.ru>
2022-10-25 14:55:03 +0300

    manufacturer секция

--- assets/template/js/main.js
+++ assets/template/js/main.js
@@ -6,7 +6,17 @@ $(document).ready(function(){
         $(this).toggleClass('active');
         $('.menu_sidebar,.menu_list').toggleClass('active')
     }
-
-
     $('.menu-btn').on('click',menuHidden);
+
+
+    // manufacturer slider
+
+    $('.manufacturer_list').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        prevArrow:$('.manufacturer-nav .slick-prev'),
+        nextArrow:$('.manufacturer-nav .slick-next'),
+        
+    });
 });
