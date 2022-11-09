0ed2a3c4e9700098956f0d9b56f761e3821c782f
locvi <locvi@yandex.ru>
2022-11-09 17:54:00 +0300

    new js edited

--- assets/template/js/configurator.js
+++ assets/template/js/configurator.js
@@ -5,55 +5,115 @@ $(document).ready(function(){
     let hardSize = $('.hard-size'); //объем жесткого диска
 
     function calculated(){
-        let headTitle = $(this).closest('.configurator_block').find('.configurator-head').text() + " : ";
+        let headTitle = $(this).closest('.configurator_block').find('.configurator-head').text();
         let radioBtnPrice =  parseInt($(this).closest('.configurator_bottom_item').find('.configurator_bottom_price').text().replace(/\s/g, ''));
         let priceText = $('.product_price');
+        let checboxesListIndex = $(this).closest('.configurator_bottom_item').index();
+
         if(isNaN(radioBtnPrice)) radioBtnPrice = 0;
-        sumOptions.set(headTitle,radioBtnPrice);
+
+        if(headTitle == "Оперативная память RAM"){
+            sumOptions.set(headTitle + " " + checboxesListIndex + " : ",radioBtnPrice);
+            if(!$(this).prop('checked')){
+                sumOptions.delete(headTitle + " " + checboxesListIndex + " : ");
+            }
+        }
+        else{
+            sumOptions.set(headTitle + " : ",radioBtnPrice);
+        }
+
 
         finallySum = productPrice + arrayCaclValues();
 
         priceText.text(finallySum.toLocaleString() + ' руб')
+    }
+
+    function checkRamCount(){
+        let currentCheckbox = $(this).prop('checked');
+        let currentRamInput = $(this).closest('.configurator_bottom_item').find('.ram-count')
+
+        if(currentCheckbox) {
+            currentRamInput.show();
+            $(this).closest('.configurator_bottom_item').find('.custom-radio').addClass('active')
+        }
+        else {
+            currentRamInput.hide();
+            $(this).closest('.configurator_bottom_item').find('.custom-radio').removeClass('active')
+        }
+    }
+
+    function ramInputValue(){
+        let maxSizeRam = parseInt($('.custom-radio-btn.active').attr('data-max-val'));
+        let current = $(this);
+        let inputParent = $('.configurator_block-ram .configurator_bottom_item').find('.custom-radio')
+        let countValus = $('.custom-radio.active').closest('.configurator_bottom_item').find('.ram-count')
+        let countMaxSizeRam = 0;
+        countMaxSizeRam = maxSizeRam;
+
+        if(current.val() >= countMaxSizeRam){
+            current.val(countMaxSizeRam)
+            inputParent.not('.active').prop('disabled',true)
+            inputParent.not('.active').closest('.configurator_bottom_item').addClass('disabled')
+        }
+        else{
+            inputParent.not('.active').prop('disabled',false)
+            inputParent.not('.active').closest('.configurator_bottom_item').removeClass('disabled')
+        }
+        countValus.each(function(i){    
+            countMaxSizeRam = countMaxSizeRam - parseInt(countValus[i].value);
+        })
         
+         
     }
 
     //фильтрация по атрибутам
-    function filter(){
-       let block = $(this),
-           blockAttr = block.val(),
-           items = block.closest('.configurator_block').find('.configurator_bottom_item'),
-           itemsInputDataManuf = items.find(`input[data-manuf="${blockAttr}"]`).closest('.configurator_bottom_item'),
-           checboxes = $(this).closest('.configurator_checkboxes').find('input')
+    // function filter(){
+    //    let block = $(this),
+    //        blockAttr = block.val(),
+    //        items = block.closest('.configurator_block').find('.configurator_bottom_item'),
+    //        itemsInputDataManuf = items.find(`input[data-manuf="${blockAttr}"]`).closest('.configurator_bottom_item'),
+    //        checboxes = $(this).closest('.configurator_checkboxes').find('input')
 
-        if(block.prop('checked')){
-            items.addClass('hidden')
-            itemsInputDataManuf.removeClass('hidden')
+    //     if(block.prop('checked')){
+    //         items.addClass('hidden')
+    //         itemsInputDataManuf.removeClass('hidden')
 
-        }
-        else if(!block.prop('checked')){
-            itemsInputDataManuf.addClass('hidden')
-        }
+    //     }
+    //     else if(!block.prop('checked')){
+    //         itemsInputDataManuf.addClass('hidden')
+    //     }
         
-        let checboxFalse = new Map();
-        checboxes.each(function(i){
-            if(!checboxes[i].checked){
-                checboxFalse.set(i,checboxes[i])
-            }
-        })
+    //     let checboxFalse = new Map();
+    //     checboxes.each(function(i){
+    //         if(!checboxes[i].checked){
+    //             checboxFalse.set(i,checboxes[i])
+    //         }
+    //     })
 
-        if(checboxes.length == checboxFalse.size){
-            items.removeClass('hidden')
-        }
-    }
+    //     if(checboxes.length == checboxFalse.size){
+    //         items.removeClass('hidden')
+    //     }
+    // }
 
-    function addProcessorNumbers(){
+    function cpuChange(){
         let titleRadioBtn = $(this).closest('.configurator_radio-block').find('label').text();
-        let cpuItems = $('.configurator_block-cpu').find('.configurator_bottom_item label');
+        let spanInCpuItem = $('.configurator_block-cpu').find('.configurator_bottom_item label span');
+        let priceCount = $('.configurator_block-cpu .configurator_bottom_price');
+        let currentBtn = $(this).closest('.configurator_btns .configurator_radio-block').index();
+        let cpuChecked = $('.configurator_block-cpu .custom-radio:checked')
 
-        cpuItems.prepend(titleRadioBtn + " x ");
-
-         
         
+
+        let priceArray = ['data-x2','data-x4','data-x8'];
+
+        priceCount.each(i => {
+            $(priceCount[i]).text(priceCount[i].getAttribute(priceArray[currentBtn]))
+        });
+
+
+        cpuChecked.click();
+        spanInCpuItem.html(titleRadioBtn);
+
     }
 
     // получаем сумму значения массива sumOptions
@@ -98,7 +158,16 @@ $(document).ready(function(){
     })
 
     $('.configurator_bottom_radio input').on('click',calculated);
-    $('.configurator_top_item input').on('click input',filter);
+    //$('.configurator_top_item input').on('click input',filter);
 
-    $('.configurator_radio-block input').on('click',addProcessorNumbers)
+    $('.configurator_radio-block input').on('click',cpuChange)
+
+    $('.configurator_block-ram .custom-radio').on('click',checkRamCount)
+
+    $('.configurator_block-ram .ram-count').on('input change',ramInputValue)
+
+    $('.custom-radio-btn').on('click',function(){
+        $('.custom-radio-btn').removeClass('active')
+        $(this).addClass('active')
+    }) 
 })
